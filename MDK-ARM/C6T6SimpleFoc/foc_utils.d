7bd425b296a5112319a45b4a2e865be52f59a7f1
yuchen <116358@qq>
2023-11-09 22:49:16 +0800

    Delete MDK.

--- MDK-ARM/C6T6SimpleFoc/foc_utils.d
+++
@@ -1,3 +0,0 @@
-c6t6simplefoc\foc_utils.o: ..\USER\foc_utils.c
-c6t6simplefoc\foc_utils.o: ..\USER\foc_utils.h
-c6t6simplefoc\foc_utils.o: D:\Keil5\ARM\ARMCC\Bin\..\include\math.h
--- MDK-ARM/C6T6SimpleFoc/focmotor.d
+++
@@ -1,6 +0,0 @@
-c6t6simplefoc\focmotor.o: ..\USER\FOCMotor.c
-c6t6simplefoc\focmotor.o: ..\USER\FOCMotor.h
-c6t6simplefoc\focmotor.o: ..\USER\foc_utils.h
-c6t6simplefoc\focmotor.o: D:\Keil5\ARM\ARMCC\Bin\..\include\math.h
-c6t6simplefoc\focmotor.o: ..\USER\BLDCmotor.h
-c6t6simplefoc\focmotor.o: ..\USER\MagneticSensor.h
